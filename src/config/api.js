62bb4245ae5a82983d9e0ca77376a8b02f427c27
aqingxin <1607440679@qq.com>
2018-10-13 15:38:48 +0800

    API接口文件

---
+++ src/config/api.js
@@ -0,0 +1,12 @@
+const Url='https://elm.cangdu.org';
+
+const urlConfig = {
+  guessCity:Url+'/v1/cities?type=guess',
+  hotCity:Url+'/v1/cities?type=hot',
+  groupCity:Url+'/v1/cities?type=group',
+  searchAddress:Url+'/v1/pois?',
+  foodClassify:Url+'/v2/index_entry',
+  shoppingList:Url+'/shopping/restaurants?',
+  search:Url+'/v4/restaurants?'
+}
+export default urlConfig;
